fa785b7cbe49a9df3b2cc018a649b1713a3f0648
pranavprakashkj <pranav@bhiveworkspace.com>
2024-01-29 13:34:02 +0530

    sec 13 finished

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import Counter from './components/Counter/Counter.jsx';
+import Header from './components/Header.jsx';
+import { log } from './log.js';
+import ConfigureCounter from './components/Counter/ConfigureCounter.jsx';
+
+function App() {
+  log('<App /> rendered');
+  const [chosenCount, setChosenCount] = useState(0);
+
+  function HandleSetCount(newCount) {
+    setChosenCount(newCount)
+  };
+
+
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfigureCounter onSet={HandleSetCount} />
+
+        <Counter key={chosenCount} initialCount={chosenCount} />
+      </main>
+    </>
+  );
+}
+
+export default App;
