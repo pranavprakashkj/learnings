4fe904fe31bc466ac5a84075a22360ceacefd048
pranavprakashkj <pranav@bhiveworkspace.com>
2024-02-09 13:44:26 +0530

    sec 22 done

---
+++ frontend/src/pages/Authentication.js
@@ -0,0 +1,47 @@
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "unsupported mode" }, { status: 422 });
+  }
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "could not authenticate" }, { status: 500 });
+  }
+
+  const resData = await response.json();
+  const token = resData.token;
+  localStorage.setItem("token", token);
+  const expire = new Date();
+  expire.setHours(expire.getHours() + 1);
+  localStorage.setItem("expiration", expire.toISOString());
+
+  return redirect("/");
+}
---
+++ frontend/src/pages/Root.js
@@ -0,0 +1,44 @@
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../utils/Auth";
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  });
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
